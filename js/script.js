55d6998d16cf88a3e2aab411b42ba607b1a48d4a
Santosh Kumar Subedi <subedisantos9@gmail.com>
2018-04-06 23:11:29 +0545

    first commit half of the project is compleated,design is change can be chect point

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function openNav() {
+    document.getElementById("header-second-div").style.width = "100%";
+}
+
+
+function closeNav(){
+document.getElementById("header-second-div").style.width = "00%";
+}
